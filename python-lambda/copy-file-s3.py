cc9315c80972977e1f6b01831e957679d90db576
namanchopra21087 <namanchopra21087@gmail.com>
2022-03-16 19:03:39 +0530

    Lambda to copy file in s3

--- python-lambda/copy-file-s3.py
+++ python-lambda/copy-file-s3.py
@@ -1,20 +1,22 @@
 import boto3
 import json
+import time
 s3 = boto3.resource('s3')
 
 def lambda_handler (event, context):
-  bucket=s3.Bucket('test-s3-dev')
-  dest_bucket=s3.Bucket('test-s3-dev')
+  bucket=s3.Bucket('mcm-s3-jumpstart-dev')
+  dest_bucket=s3.Bucket('mcm-s3-jumpstart-dev')
   print(dest_bucket)
   print(bucket)
-  x=10003
-  for obj in bucket.objects.filter(Prefix='file-transfer-key’):
+  x=0
+  for obj in bucket.objects.filter(Prefix='jumpstart-to-nahan-sftp-transfer-dev'):
       dest_key=obj.key.split('dev/')[1]
       if not dest_key=="":
-        dest_key=dest_key.replace('XYZ','XYZ'+str(x))
-        dest_key='file-transfer-key/'+dest_key
+        ts=time.time()
+        dest_key=dest_key.replace('XYZ','XYZ'+str(ts))
+        dest_key='jumpstart-to-nahan-sftp-transfer-dev/'+dest_key
         s3.Object(dest_bucket.name,dest_key).copy_from(CopySource= { 'Bucket': obj.bucket_name , 'Key' : obj.key})
-        x+=1
-        if x>250001:
+        x=x+1
+        if x>538:
           break
   print("Value of x:"+str(x))
